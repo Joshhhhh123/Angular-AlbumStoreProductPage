1e29515fd2e76ae3217aa7139f1b64ca3a6bca59
Joshhhhh123 <129487721+Joshhhhh123@users.noreply.github.com>
2023-04-02 09:25:08 +0800

    qwe

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from '../album';
 import { ProductService } from '../product.service';
 
 @Component({
@@ -8,13 +9,13 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albumInfo;
+  albumInfo: Album;
 
   constructor( private _productService: ProductService ) { }
 
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response =>
-    this.albumInfo = response);
+      this.albumInfo = response);
   }
 
 }
